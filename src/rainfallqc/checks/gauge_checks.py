7bd087e22648e01c045523739f507eb23abca05a
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-18 16:45:20 +0100

    add QC2

--- src/rainfallqc/checks/gauge_checks.py
+++ src/rainfallqc/checks/gauge_checks.py
@@ -31,3 +31,28 @@ def get_years_where_nth_percentile_is_zero(data: pl.DataFrame, rain_col: str, qu
     """
     nth_perc = data.group_by_dynamic("time", every="1y").agg(pl.quantile(rain_col, quantile))
     return nth_perc.filter(pl.col(rain_col) == 0)["time"].dt.year().to_list()
+
+
+def get_years_where_annual_mean_k_top_rows_are_zero(data: pl.DataFrame, rain_col: str, k: int) -> list:
+    """
+    Return year list where the annual mean top-K rows are zero.
+
+    This QC2 from the IntenseQC framework
+
+    Parameters
+    ----------
+    data :
+        Rainfall data
+    rain_col :
+        Column with rainfall data
+    k :
+        Number of top values check i.e. k==5 is top 5
+
+    Returns
+    -------
+    year_list :
+        List of years where k-largest are zero.
+
+    """
+    data_top_k = data.group_by_dynamic("time", every="1y").agg(pl.col(rain_col).top_k(k).min())
+    return data_top_k.filter(pl.col(rain_col) == 0)["time"].dt.year().to_list()
--- tests/unit/test_gauge_checks.py
+++ tests/unit/test_gauge_checks.py
@@ -13,14 +13,20 @@ def test_get_years_where_nth_percentile_is_zero(daily_gdsr_data):
     years_95th = gauge_checks.get_years_where_nth_percentile_is_zero(
         daily_gdsr_data, rain_col=DEFAULT_RAIN_COL, quantile=0.95
     )
-    years_99th = gauge_checks.get_years_where_nth_percentile_is_zero(
-        daily_gdsr_data, rain_col=DEFAULT_RAIN_COL, quantile=0.99
-    )
     years_50th = gauge_checks.get_years_where_nth_percentile_is_zero(
         daily_gdsr_data, rain_col=DEFAULT_RAIN_COL, quantile=0.5
     )
 
     assert len(years_95th) == 0
-    assert len(years_99th) == 0
-    assert len(years_50th) != 0
     numpy.testing.assert_array_equal(years_50th, [2006, 2007, 2008, 2009, 2010])
+
+
+def test_get_years_where_annual_mean_k_top_rows_are_zero(daily_gdsr_data):
+    years_k_top_5 = gauge_checks.get_years_where_annual_mean_k_top_rows_are_zero(
+        daily_gdsr_data, rain_col=DEFAULT_RAIN_COL, k=5
+    )
+    years_k_top_1600 = gauge_checks.get_years_where_annual_mean_k_top_rows_are_zero(
+        daily_gdsr_data, rain_col=DEFAULT_RAIN_COL, k=1600
+    )
+    assert len(years_k_top_5) == 0
+    numpy.testing.assert_array_equal(years_k_top_1600, [2006, 2008, 2010])
