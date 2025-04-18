01c9058e2b1db49ad3d0013de2a787aad2de4e58
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-18 11:03:25 +0100

    add a conftest and put data fixtures here

---
+++ tests/conftest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+"""Pytest config file."""
+import numpy as np
+import polars as pl
+import pytest
+
+from rainfallqc.utils import data_loaders
+
+MULTIPLYING_FACTORS = {"hourly": 24, "daily": 1}  # compared to daily reference
+
+
+@pytest.fixture
+def random() -> np.random.Generator:
+    return np.random.default_rng(seed=list(map(ord, "𝕽𝔞𝖓𝔡𝖔𝔪")))
+
+
+@pytest.fixture
+def daily_gdsr_data() -> pl.DataFrame:
+    # TODO: maybe randomise this with every call? Or use parameterise
+    data_path = "./tests/test_data/GDSR/DE_02483.txt"
+    # read in metadata of gauge
+    gdsr_metadata = data_loaders.read_gdsr_metadata(data_path)
+    rain_col = f"rain_{gdsr_metadata['original_units']}"
+
+    # read in gauge data
+    gdsr_data = pl.read_csv(
+        data_path,
+        skip_rows=20,
+        schema_overrides={rain_col: pl.Float64},
+    )
+
+    # add datetime column to data
+    data_loaders.add_datetime_to_gdsr_data(
+        gdsr_data, gdsr_metadata, multiplying_factor=MULTIPLYING_FACTORS["hourly"]
+    )
+    return gdsr_data
--- tests/unit/test_gauge_checks.py
+++ tests/unit/test_gauge_checks.py
@@ -2,37 +2,9 @@
 
 """Tests for rain gauge quality control checks."""
 
-import polars as pl
-import pytest
 
 from rainfallqc.checks import gauge_checks
-from rainfallqc.utils import data_loaders
-
-MULTIPLYING_FACTORS = {"hourly": 24, "daily": 1}  # compared to daily reference
 
 
-@pytest.fixture
-def test_daily_gdsr_data():
-    data_path = (
-        "../test_data/GDSR/DE_02483.txt",
-    )  # TODO: maybe randomise this with every call? Or use parameterise?
-    # read in metadata of gauge
-    gdsr_metadata = data_loaders.read_gdsr_metadata(data_path)
-    rain_col = f"rain_{gdsr_metadata['original_units']}"
-
-    # read in gauge data
-    gdsr_data = pl.read_csv(
-        data_path,
-        skip_rows=20,
-        schema_overrides={rain_col: pl.Float64},
-    )
-
-    # add datetime column to data
-    data_loaders.add_datetime_to_gdsr_data(
-        gdsr_data, gdsr_metadata, multiplying_factor=MULTIPLYING_FACTORS["hourly"]
-    )
-    return gdsr_data
-
-
-def test_get_years_where_percentiles_are_zero(test_daily_gdsr_data):
-    gauge_checks.get_years_where_percentiles_are_zero(test_daily_gdsr_data)
+def test_get_years_where_percentiles_are_zero(daily_gdsr_data):
+    gauge_checks.get_years_where_percentiles_are_zero(daily_gdsr_data)
