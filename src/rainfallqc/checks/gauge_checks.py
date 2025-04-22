76e1d16aedd2d4b329e286f4895be43cb3f86613
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-22 11:45:17 +0100

    add comment

--- src/rainfallqc/checks/gauge_checks.py
+++ src/rainfallqc/checks/gauge_checks.py
@@ -128,9 +128,8 @@ def intermittency_check(
 
     """
     # 1. Identify missing values
-    data = data_utils.replace_missing_vals_with_nan_gdsr_data(data, rain_col)
+    data = data_utils.replace_missing_vals_with_nan_gdsr_data(data, rain_col)  # drops None by default
     missing_vals_mask = data[rain_col].is_nan()
-    print(f"{missing_vals_mask.sum()} missing values")
     data = data.with_columns(missing_vals_mask.alias("is_missing"))
 
     # 2. Identify group numbers for consecutive nulls
