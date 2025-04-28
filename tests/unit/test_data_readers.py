c128d0a6d88af8b713a7eb49f83821acade5ad17
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-28 16:23:06 +0100

    add all etcddi checks

--- tests/unit/test_data_readers.py
+++ tests/unit/test_data_readers.py
@@ -7,5 +7,25 @@ from rainfallqc.utils import data_readers
 # test when start_datetime not found
 
 
+def test_load_CDD_ETCCDI_data():
+    data_readers.load_ETCCDI_data(etccdi_var="CDD")
+
+
+def test_load_CWD_ETCCDI_data():
+    data_readers.load_ETCCDI_data(etccdi_var="CWD")
+
+
+def test_load_PRCPTOT_ETCCDI_data():
+    data_readers.load_ETCCDI_data(etccdi_var="PRCPTOT")
+
+
 def test_load_R99p_ETCCDI_data():
     data_readers.load_ETCCDI_data(etccdi_var="R99p")
+
+
+def test_load_Rx1day_ETCCDI_data():
+    data_readers.load_ETCCDI_data(etccdi_var="Rx1day")
+
+
+def test_load_SDII_ETCCDI_data():
+    data_readers.load_ETCCDI_data(etccdi_var="SDII")
