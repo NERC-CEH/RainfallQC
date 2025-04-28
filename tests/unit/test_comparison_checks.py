da53d8e6111785fb5ea6df27d544505c58ab66a1
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-28 16:20:50 +0100

    add data

--- tests/unit/test_comparison_checks.py
+++ tests/unit/test_comparison_checks.py
@@ -7,5 +7,5 @@ from rainfallqc.checks import comparison_checks
 DEFAULT_RAIN_COL = "rain_mm"
 
 
-def test_R99_check():
-    comparison_checks.check_annual_exceedance_ETCCDI_R99p()
+def test_R99_check(daily_gdsr_data):
+    comparison_checks.check_annual_exceedance_ETCCDI_R99p(daily_gdsr_data, rain_col=DEFAULT_RAIN_COL)
