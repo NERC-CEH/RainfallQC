cdf049bb148ff6dd386566556ab6fe129e31d69e
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-29 10:32:47 +0100

    add tests for exceedance

--- tests/unit/test_comparison_checks.py
+++ tests/unit/test_comparison_checks.py
@@ -2,6 +2,8 @@
 
 """Tests for rain gauge quality control checks."""
 
+import pytest
+
 from rainfallqc.checks import comparison_checks
 
 DEFAULT_RAIN_COL = "rain_mm"
@@ -14,3 +16,10 @@ def test_R99_check(daily_gdsr_data, daily_gdsr_metadata):
         gauge_lat=daily_gdsr_metadata["latitude"],
         gauge_lon=daily_gdsr_metadata["longitude"],
     )
+
+
+@pytest.mark.parametrize(
+    "vals,max_ref_val,expected", [(1.5, 1, 4), (1.33, 1, 3), (1.2, 1, 2), (1.1, 1, 1), (0.9, 1, 0)]
+)
+def test_exceedance_check(vals, max_ref_val, expected):
+    assert comparison_checks.exceedance_check(vals, max_ref_val) == expected
