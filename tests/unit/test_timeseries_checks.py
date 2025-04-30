cb84ddf4c5904b59cc963028db2f45bd862cea31
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-30 15:16:40 +0100

    add ts check

--- tests/unit/test_timeseries_checks.py
+++ tests/unit/test_timeseries_checks.py
@@ -8,4 +8,6 @@ DEFAULT_RAIN_COL = "rain_mm"
 
 
 def test_dry_period_cdd_check(hourly_gdsr_data, gdsr_metadata):
-    timeseries_checks.dry_period_cdd_check(hourly_gdsr_data, rain_col=DEFAULT_RAIN_COL)
+    timeseries_checks.dry_period_cdd_check(
+        hourly_gdsr_data, rain_col=DEFAULT_RAIN_COL, gauge_lat=gdsr_metadata["lat"], gauge_lon=gdsr_metadata["lon"]
+    )
