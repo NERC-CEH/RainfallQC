1a5bd5c6676472c4fd5b5b465cc9e8f48a845754
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-04-30 15:22:46 +0100

    put most method in func for now

--- src/rainfallqc/checks/timeseries_checks.py
+++ src/rainfallqc/checks/timeseries_checks.py
@@ -48,7 +48,45 @@ def dry_period_cdd_check(
     # 3. Get nearest local CDD value to the gauge coordinates
     nearby_dry_spell_days = neighbourhood_utils.get_nearest_etccdi_val_to_gauge(dry_spell_days, gauge_lat, gauge_lon)
 
-    return data[rain_col], nearby_dry_spell_days
+    # 4. Get local maximum ETCCDI value
+    max_nearby_dry_spell = np.max(nearby_dry_spell_days["CDD_days"])
+
+    # get dry spells and their legnths
+    gauge_dry_spells = data.with_columns(
+        (pl.col(rain_col) == 0).cast(pl.Int8()).alias("is_zero"),
+    )
+
+    gauge_dry_spell_groups = gauge_dry_spells.with_columns(
+        ((pl.col("is_zero").diff().fill_null(0) == 1).cum_sum()).alias("group_id")
+    )
+
+    gauge_dry_spell_lengths = (
+        gauge_dry_spell_groups.filter(pl.col("is_zero") == 1)
+        .group_by("group_id")
+        .agg(
+            pl.first("time").alias("dry_spell_start"),
+            pl.last("time").alias("dry_spell_end"),
+            pl.col("is_zero").sum().alias("dry_spell_length_h"),
+        )
+        .sort("group_id")
+    )
+
+    gauge_dry_spell_lengths_in_days = gauge_dry_spell_lengths["dry_spell_length_h"] / 24
+
+    # May need to rethink how this is done uniformly (as could use day check)
+    gauge_dry_spell_lengths_flags = gauge_dry_spell_lengths.with_columns(
+        pl.when(pl.col("dry_spell_length_h") / 24 >= max_nearby_dry_spell * 1.5)
+        .then(4)
+        .when(pl.col("dry_spell_length_h") / 24 >= max_nearby_dry_spell * 1.33)
+        .then(3)
+        .when(pl.col("dry_spell_length_h") / 24 >= max_nearby_dry_spell * 1.2)
+        .then(2)
+        .when(pl.col("dry_spell_length_h") / 24 >= max_nearby_dry_spell)
+        .then(1)
+        .otherwise(0)
+        .alias("dry_spell_flag")
+    )
+    return gauge_dry_spell_lengths_flags, gauge_dry_spell_lengths_in_days
 
 
 def compute_dry_spell_days(dry_spell_data: xr.Dataset) -> xr.Dataset:
