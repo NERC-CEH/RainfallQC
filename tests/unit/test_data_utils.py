a5741ad5c8e8d002741ae04e2b6f1f958468250a
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-08 10:30:50 +0100

    add data util check

--- tests/unit/test_data_utils.py
+++ tests/unit/test_data_utils.py
@@ -14,13 +14,16 @@ DEFAULT_RAIN_COL = "rain_mm"
 def test_check_data_has_consistent_time_step(hourly_gdsr_data, inconsistent_timestep_data):
     data_utils.check_data_has_consistent_time_step(hourly_gdsr_data)
     with pytest.raises(ValueError):
-        data_utils.check_data_has_consistent_time_step(
-            data_utils.check_data_has_consistent_time_step(inconsistent_timestep_data)
-        )
+        data_utils.check_data_has_consistent_time_step(inconsistent_timestep_data)
 
 
 def test_get_data_timesteps(hourly_gdsr_data, inconsistent_timestep_data):
-    data_utils.get_data_timesteps(hourly_gdsr_data)
+    result = data_utils.get_data_timesteps(hourly_gdsr_data)
+    assert len(result) == 1
+    assert result[0] == datetime.timedelta(seconds=3600)
+    result = data_utils.get_data_timesteps(inconsistent_timestep_data)
+    assert len(result) == 3
+    assert result[0] == datetime.timedelta(seconds=60)
 
 
 def test_format_timedelta_duration():
@@ -35,4 +38,7 @@ def test_format_timedelta_duration():
 
 
 def test_get_data_timestep_as_str(hourly_gdsr_data, inconsistent_timestep_data):
-    data_utils.get_data_timestep_as_str(hourly_gdsr_data)
+    result = data_utils.get_data_timestep_as_str(hourly_gdsr_data)
+    assert result == "1h"
+    with pytest.raises(ValueError):
+        data_utils.get_data_timestep_as_str(inconsistent_timestep_data)
