17a097599e5bea8c54f4214f350a6170e0926a03
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-12 15:34:40 +0100

    replace with pl version

--- tests/unit/test_neighbourhood_utils.py
+++ tests/unit/test_neighbourhood_utils.py
@@ -2,6 +2,8 @@
 
 """Tests for neighbourhood utility functions."""
 
+import polars as pl
+
 from rainfallqc.utils import neighbourhood_utils
 
 
@@ -9,5 +11,5 @@ def test_compute_distance_from_target_id(gdsr_gauge_network):
     result = neighbourhood_utils.compute_km_distance_from_target_id(
         gauge_network_metadata=gdsr_gauge_network, target_id="DE_00310"
     )
-    assert round(result["DE_02483"], 2) == 13.13
-    assert round(result["DE_00310"], 2) == 0.0
+    assert round(result.filter(pl.col("station_id") == "DE_02483")["distances"][0], 2) == 13.13
+    assert round(result.filter(pl.col("station_id") == "DE_00310")["distances"][0], 2) == 0.0
