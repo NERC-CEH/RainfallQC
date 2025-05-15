d91b4111cf4b4d2220bae147618ba5a86b5f8134
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-15 14:33:08 +0100

    add outline for wet nieghbour check

--- src/rainfallqc/checks/neighbourhood_checks.py
+++ src/rainfallqc/checks/neighbourhood_checks.py
@@ -6,3 +6,20 @@ Neighbourhood checks are QC checks that: "detect abnormalities in a gauges given
 
 Classes and functions ordered by appearance in IntenseQC framework.
 """
+
+
+# def wet_neighbour_check(target_gauge_id: str, gauge_network_path: str, time_res: str) -> pl.DataFrame:
+#     """
+#     Run neighbour check (wet) for hourly or daily data.
+#
+#     Parameters
+#     ----------
+#     target_gauge_id :
+#     time_res
+#
+#     Returns
+#     -------
+#
+#     """
+#     # data_utils.check_data_is_specific_time_res(data, time_res)
+#     return target_gauge_id
--- tests/unit/test_neighbourhood_checks.py
+++ tests/unit/test_neighbourhood_checks.py
@@ -2,13 +2,17 @@
 
 """Tests for neighbourhood quality control checks."""
 
-import numpy.testing
-import pytest
-
-from rainfallqc.checks import neighbourhood_checks
+# from rainfallqc.checks import neighbourhood_checks
 
 DEFAULT_RAIN_COL = "rain_mm"
 DISTANCE_THRESHOLD = 50  # 50 km
 OVERLAP_THRESHOLD = 365 * 3  # three years
 
-test = (pytest, numpy.testing, neighbourhood_checks)
+
+# def test_wet_neighbour_check(hourly_gdsr_data):
+#     neighbourhood_checks.wet_neighbour_check(hourly_gdsr_data, time_res="hourly")
+#
+#
+# def test_wet_neighbour_check_daily(daily_gdsr_data, daily_gpcc_data):
+#     neighbourhood_checks.wet_neighbour_check(daily_gdsr_data, time_res="daily")
+#     neighbourhood_checks.wet_neighbour_check(daily_gpcc_data, time_res="daily")
