98eb5af3a2787c111b2ce636f1139c862578c363
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-05-19 13:24:51 +0100

    seperate flag funcs

--- src/rainfallqc/checks/neighbourhood_checks.py
+++ src/rainfallqc/checks/neighbourhood_checks.py
@@ -51,49 +51,8 @@ def wet_neighbour_check(
         all_neighbour_data = data_readers.convert_gdsr_hourly_to_daily(all_neighbour_data, rain_cols=rain_cols)
 
     for neighbouring_gauge_col in neighbouring_gauge_cols:
-        diff_neighbour_col = f"diff_{neighbouring_gauge_col}"
-
-        # 2. Remove nans from target and neighbour
-        all_neighbour_data_clean = all_neighbour_data.drop_nans(subset=[target_gauge_col, neighbouring_gauge_col])
-
-        # 3. Get normalised difference between target and neighbour
-        all_neighbour_data_diff = normalised_diff_between_target_neighbours(
-            all_neighbour_data_clean, target_gauge_col, neighbouring_gauge_col
-        )
-
-        # 4. filter wet values values
-        all_neighbour_data_filtered_diff = all_neighbour_data_diff.filter(
-            (pl.col(target_gauge_col) >= wet_threshold)
-            & (pl.col(target_gauge_col).is_finite())
-            & (pl.col(neighbouring_gauge_col).is_finite())
-            & (pl.col(diff_neighbour_col) > 0.0)
-        )
-
-        # 5. Fit exponential function of normalised diff and get q95, q99 and q999
-        expon_perc = stats.fit_expon_and_get_percentile(
-            all_neighbour_data_filtered_diff[diff_neighbour_col], percentiles=[0.95, 0.99, 0.999]
-        )
-
-        # 6. Assign flags
-        all_neighbour_data_wet_flags = all_neighbour_data_diff.with_columns(
-            pl.when((pl.col(target_gauge_col) >= wet_threshold) & (pl.col(diff_neighbour_col) <= expon_perc[0.95]))
-            .then(0)
-            .when(
-                (pl.col(target_gauge_col) >= wet_threshold)
-                & (pl.col(diff_neighbour_col) > expon_perc[0.95])
-                & (pl.col(diff_neighbour_col) <= expon_perc[0.99]),
-            )
-            .then(1)
-            .when(
-                (pl.col(target_gauge_col) >= wet_threshold)
-                & (pl.col(diff_neighbour_col) > expon_perc[0.99])
-                & (pl.col(diff_neighbour_col) <= expon_perc[0.999]),
-            )
-            .then(2)
-            .when((pl.col(target_gauge_col) >= wet_threshold) & (pl.col(diff_neighbour_col) > expon_perc[0.999]))
-            .then(3)
-            .otherwise(0)
-            .alias(f"wet_flags_{neighbouring_gauge_col}")
+        all_neighbour_data_wet_flags = flag_wet_day_errors_based_on_neighbours(
+            all_neighbour_data, target_gauge_col, neighbouring_gauge_col, wet_threshold
         )
 
         # 6. Join to all data
@@ -110,15 +69,115 @@ def wet_neighbour_check(
     return all_neighbour_data
 
 
+def flag_wet_day_errors_based_on_neighbours(
+    all_neighbour_data: pl.DataFrame, target_gauge_col: str, neighbouring_gauge_col: str, wet_threshold: float
+) -> pl.DataFrame:
+    """
+    Flag wet days with errors based on the percentile difference with neighbouring gauge.
+
+    Parameters
+    ----------
+    all_neighbour_data :
+        Rainfall data of all neighbouring gauges with time col
+    target_gauge_col :
+        Target gauge column
+    neighbouring_gauge_col:
+        Neighbouring gauge column
+    wet_threshold :
+        Threshold for rainfall intensity in given time period
+
+    Returns
+    -------
+    all_neighbour_data_wet_flags :
+        Data with wet flags
+
+    """
+    # 1. get diff column
+    diff_neighbour_col = f"diff_{neighbouring_gauge_col}"
+
+    # 2. Remove nans from target and neighbour
+    all_neighbour_data_clean = all_neighbour_data.drop_nans(subset=[target_gauge_col, neighbouring_gauge_col])
+
+    # 3. Get normalised difference between target and neighbour
+    all_neighbour_data_diff = normalised_diff_between_target_neighbours(
+        all_neighbour_data_clean, target_gauge_col=target_gauge_col, neighbouring_gauge_col=neighbouring_gauge_col
+    )
+    # 4. filter wet values
+    all_neighbour_data_filtered_diff = filter_data_based_on_unusual_wetness(
+        all_neighbour_data_diff,
+        target_gauge_col=target_gauge_col,
+        neighbouring_gauge_col=neighbouring_gauge_col,
+        wet_threshold=wet_threshold,
+    )
+
+    # 5. Fit exponential function of normalised diff and get q95, q99 and q999
+    expon_perc = stats.fit_expon_and_get_percentile(
+        all_neighbour_data_filtered_diff[diff_neighbour_col], percentiles=[0.95, 0.99, 0.999]
+    )
+    # 6. Assign flags
+    all_neighbour_data_wet_flags = all_neighbour_data_diff.with_columns(
+        pl.when((pl.col(target_gauge_col) >= wet_threshold) & (pl.col(diff_neighbour_col) <= expon_perc[0.95]))
+        .then(0)
+        .when(
+            (pl.col(target_gauge_col) >= wet_threshold)
+            & (pl.col(diff_neighbour_col) > expon_perc[0.95])
+            & (pl.col(diff_neighbour_col) <= expon_perc[0.99]),
+        )
+        .then(1)
+        .when(
+            (pl.col(target_gauge_col) >= wet_threshold)
+            & (pl.col(diff_neighbour_col) > expon_perc[0.99])
+            & (pl.col(diff_neighbour_col) <= expon_perc[0.999]),
+        )
+        .then(2)
+        .when((pl.col(target_gauge_col) >= wet_threshold) & (pl.col(diff_neighbour_col) > expon_perc[0.999]))
+        .then(3)
+        .otherwise(0)
+        .alias(f"wet_flags_{neighbouring_gauge_col}")
+    )
+    return all_neighbour_data_wet_flags
+
+
+def filter_data_based_on_unusual_wetness(
+    neighbour_data_diff: pl.DataFrame, target_gauge_col: str, neighbouring_gauge_col: str, wet_threshold: float
+) -> pl.DataFrame:
+    """
+    Filter data based on wet threshold.
+
+    Parameters
+    ----------
+    neighbour_data_diff :
+        Data with normalised diff to neighbour
+    target_gauge_col :
+        Target gauge column
+    neighbouring_gauge_col :
+        Neighbouring gauge column
+    wet_threshold :
+        Threshold for rainfall intensity in given time period
+
+    Returns
+    -------
+    filtered_diff :
+        Data filtered to wet threshold and where diff is positive (thus more wet)
+
+    """
+    return neighbour_data_diff.filter(
+        (pl.col(target_gauge_col) >= wet_threshold)
+        & (pl.col(target_gauge_col).is_finite())
+        & (pl.col(neighbouring_gauge_col).is_finite())
+        & (pl.col(f"diff_{neighbouring_gauge_col}") > 0.0)
+    )
+
+
 def normalised_diff_between_target_neighbours(
-    all_neighbour_data: pl.DataFrame, target_gauge_col: str, neighbouring_gauge_col: str
+    neighbour_data: pl.DataFrame, target_gauge_col: str, neighbouring_gauge_col: str
 ) -> pl.DataFrame:
     """
     Normalised difference between target rain col and neighbouring rain col.
 
     Parameters
     ----------
-    all_neighbour_data :
+    neighbour_data :
         Rainfall data of all neighbouring gauges with time col
     target_gauge_col :
         Target gauge column
@@ -131,7 +190,7 @@ def normalised_diff_between_target_neighbours(
         Data with normalised diff to each neighbour
 
     """
-    return all_neighbour_data.with_columns(
+    return neighbour_data.with_columns(
         (
             data_utils.normalise_data(pl.col(target_gauge_col))
             - data_utils.normalise_data(pl.col(neighbouring_gauge_col))
