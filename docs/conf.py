c5673a608b6cd1fc38f3e34a76eb6e0ab7e85ed1
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-08-27 15:49:34 +0100

    update

--- docs/conf.py
+++ docs/conf.py
@@ -74,7 +74,7 @@ release = rainfallqc.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 autosummary_generate = False
 
