bbd462b788b2387dd102ffeaa04e33183dbc1f51
Thomasjkeel <thomasjames.keel@googlemail.com>
2025-10-07 16:30:15 +0100

    add qc check to all qc checks

--- tests/unit/test_all_checks.py
+++ tests/unit/test_all_checks.py
@@ -8,7 +8,7 @@ from rainfallqc.core import all_qc_checks
 def test_all_qc_checks():
     """Test that all QC checks are registered."""
     assert isinstance(all_qc_checks.QC_CHECKS, dict)
-    assert len(all_qc_checks.QC_CHECKS) == 6  # Update this if more checks are added
+    assert len(all_qc_checks.QC_CHECKS) == 22  # Update this if more checks are added
     for name, func in all_qc_checks.QC_CHECKS.items():
         assert callable(func), f"QC check '{name}' is not callable."
         assert func.__name__ == name, f"QC check function name '{func.__name__}' does not match registry name '{name}'."
