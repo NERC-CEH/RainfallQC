84293d7733ddc6fd03e4ec117029e05aea135226
Thomasjkeel <thomasjames.keel@googlemail.com>
2026-01-28 11:31:18 +0000

    Bump version: 0.3.2 → 0.4.0-alpha

--- src/rainfallqc/__init__.py
+++ src/rainfallqc/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Tom Keel"""
 __email__ = "tomkee@ceh.ac.uk"
-__version__ = "0.3.2"
+__version__ = "0.4.0-alpha"
 
 from rainfallqc import core, utils
 from rainfallqc.checks import (
